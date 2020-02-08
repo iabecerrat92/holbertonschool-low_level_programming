63d6108f4c5f39d92512bdba8d7ab39273a649ee
iabecerrat92 <1518@holbertonschool.com>
2020-02-08 03:24:08 +0000

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers
+ * Description: For loop that prints the numbers from 00 to 99 without repeat
+ * Return: 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '8'; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 != '8' || num2 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
